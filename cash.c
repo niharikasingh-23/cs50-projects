493a66e9756c93b70832cd76a8fd1ef341aebd9b
Niharika Singh <niharikasingh2324@gmail.com>
2026-04-05 23:05:17 +0530

    Add files via upload

---
+++ cash.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int cents;
+    do
+    {
+        cents = get_int("Change owed: ");
+    }
+    while (cents < 0);
+
+    int coins = 0;
+
+    while (cents >= 25)
+    {
+        cents -= 25;
+        coins++;
+    }
+
+    while (cents >= 10)
+    {
+        cents -= 10;
+        coins++;
+    }
+
+    while (cents >= 5)
+    {
+        cents -= 5;
+        coins++;
+    }
+
+    while (cents >= 1)
+    {
+        cents -= 1;
+        coins++;
+    }
+
+    printf("%i\n", coins);
+}
